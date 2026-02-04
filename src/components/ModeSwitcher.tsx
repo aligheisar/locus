3b28972ed94025e9fc1dddb80bd4127701d15c52
Ali Gheisar <ali.gheisar.dev@gmail.com>
2026-02-04 09:37:07 +0330

    update size class on mode switcher.

--- src/components/ModeSwitcher.tsx
+++ src/components/ModeSwitcher.tsx
@@ -25,11 +25,11 @@ export function ModeSwitcher() {
         }}
       >
         <HugeiconsIcon
-          className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+          className="size-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
           icon={Sun}
         />
         <HugeiconsIcon
-          className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+          className="absolute size-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
           icon={Moon}
         />
         <span className="sr-only">Toggle theme</span>
