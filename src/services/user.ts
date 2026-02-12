d7de83cfbfe550531c9acebc1ba4001d0ff939ff
Ali Gheisar <ali.gheisar.dev@gmail.com>
2026-02-12 23:54:40 +0330

    add services for profile and user table.

---
+++ src/services/user.ts
@@ -0,0 +1,13 @@
+import type { SignupFormType } from "@/features/signup/schemas/signup-form";
+
+import { createAccount } from "@/repositories/account";
+import { createProfile } from "@/repositories/profile";
+import { createUser } from "@/repositories/user";
+
+const signupUser = async (user: SignupFormType) => {
+  const [{ userId }] = await createUser(user.email);
+  await createProfile(user.username, userId);
+  await createAccount(user.password, userId);
+};
+
+export { signupUser };
