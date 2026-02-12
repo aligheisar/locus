3e5bda18bd2ecf447db441a2aac7dc0ca2a6f398
Ali Gheisar <ali.gheisar.dev@gmail.com>
2026-02-12 16:50:34 +0330

    fix: use new path for password page in email form hook.

--- src/features/signup/hooks/use-email-form.ts
+++ src/features/signup/hooks/use-email-form.ts
@@ -24,7 +24,7 @@ const useEmailForm = () => {
     updateFormData(data);
 
     startTransition(() => {
-      router.push("/signup/generate-password");
+      router.push("/signup/password");
     });
   };
 
