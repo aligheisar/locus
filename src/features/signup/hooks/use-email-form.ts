edb0a3f6ca95e8c96857cac3090a551b4471762a
Ali Gheisar <ali.gheisar.dev@gmail.com>
2026-02-12 15:35:27 +0330

    use new context in email form.

--- src/features/signup/hooks/use-email-form.ts
+++ src/features/signup/hooks/use-email-form.ts
@@ -5,21 +5,23 @@ import { useForm } from "react-hook-form";
 import * as v from "valibot";
 
 import { signupFormSchema } from "@/features/signup/schemas/signup-form";
+import { useSignup } from "@/features/signup/hooks/use-signup";
 
 const useEmailForm = () => {
   const router = useRouter();
+  const { formData, updateData: updateFormData } = useSignup();
   const emailSchema = v.pick(signupFormSchema, ["email"]);
   type EmailFormSchema = v.InferOutput<typeof emailSchema>;
 
   const form = useForm({
     defaultValues: {
-      email: "",
+      email: formData.email ?? "",
     },
     resolver: valibotResolver(emailSchema),
   });
 
   const handleFormSubmit = async (data: EmailFormSchema) => {
-    console.log(data);
+    updateFormData(data);
 
     startTransition(() => {
       router.push("/signup/generate-password");
