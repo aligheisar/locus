8ce9487f9ea4325278d82c7f6baf409b9a303527
Ali Gheisar <ali.gheisar.dev@gmail.com>
2026-02-12 08:58:30 +0330

    add layout for signup page.

---
+++ src/app/(auth)/signup/layout.tsx
@@ -0,0 +1,7 @@
+const SignupLayout = ({ children }: LayoutProps<"/signup">) => {
+  return (
+    <main className="justify-center-safe flex items-center">{children}</main>
+  );
+};
+
+export default SignupLayout;
