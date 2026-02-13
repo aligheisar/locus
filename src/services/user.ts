f4fcccc91f84a543300ee869056f72eb29273d9a
Ali Gheisar <ali.gheisar.dev@gmail.com>
2026-02-13 16:11:07 +0330

    add logout functionality in user services.

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,7 +1,12 @@
-import { createSession } from "@/lib/session";
+import { cookies } from "next/headers";
+import { eq } from "drizzle-orm";
+
+import { createSession, verifySession } from "@/lib/session";
 
 import type { SignupFormType } from "@/features/signup/schemas/signup-form";
 
+import { db } from "@/db";
+import { sessionsTable } from "@/db/schema/sessions";
 import { createAccount } from "@/repositories/account";
 import { createProfile } from "@/repositories/profile";
 import { createUser } from "@/repositories/user";
@@ -13,4 +18,21 @@ const signupUser = async (user: SignupFormType) => {
   await createSession(userId);
 };
 
-export { signupUser };
+const logoutUser = async (): Promise<
+  { success: true } | { success: false; error: string }
+> => {
+  const session = await verifySession();
+  if (!session) return { error: "no-session", success: false };
+
+  await db
+    .update(sessionsTable)
+    .set({ revokedAt: new Date() })
+    .where(eq(sessionsTable.id, session.sessionId));
+
+  const cookieStore = await cookies();
+  cookieStore.delete("session");
+
+  return { success: true };
+};
+
+export { signupUser, logoutUser };
