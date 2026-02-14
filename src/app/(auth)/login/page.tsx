90e13afb69e4915f0823dd6613445ceb43b7cb49
Ali Gheisar <ali.gheisar.dev@gmail.com>
2026-02-14 16:00:33 +0330

    add login page.

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,13 @@
+import { Main } from "@/components/Main";
+
+import { LoginForm } from "@/features/login/components/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <Main className="place-items-center">
+      <LoginForm />
+    </Main>
+  );
+};
+
+export default LoginPage;
