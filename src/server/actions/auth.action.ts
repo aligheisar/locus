e32159e61f79a62fdfcde7eb602276c96eb49aba
Ali Gheisar <ali.gheisar.dev@gmail.com>
2026-02-24 13:02:55 +0330

    move validation logic to service layer, use new error handling method.

--- src/server/actions/auth.action.ts
+++ src/server/actions/auth.action.ts
@@ -1,70 +1,80 @@
 "use server";
 
 import { cookies, headers } from "next/headers";
-import { safeParse } from "valibot";
+
+import { err, ok } from "@/utils/error";
 
 import { authService } from "@/server/containers/auth.container";
-import { loginFormSchema } from "@/shared/schemas/login-form";
-import { signupFormSchema } from "@/shared/schemas/signup-form";
 
 const signupUserAction = async (user: unknown) => {
-  const validatedData = safeParse(signupFormSchema, user);
-  if (!validatedData.success) return;
-
   const reqHeaders = await headers();
   const meta = {
     ipAddress: reqHeaders.get("X-Forwarded-For")?.split(",")[0].trim() ?? null,
     userAgent: reqHeaders.get("User-Agent"),
   };
 
-  const session = await authService.signup(validatedData.output, meta);
+  const [error, session] = await authService.signup(user, meta);
 
-  const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
-  const cookieStore = await cookies();
-  cookieStore.set("session", session, {
-    expires: expiresAt,
-    httpOnly: true,
-    path: "/",
-    sameSite: "lax",
-    secure: true,
-  });
+  if (!error) {
+    const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+    const cookieStore = await cookies();
+    cookieStore.set("session", session, {
+      expires: expiresAt,
+      httpOnly: true,
+      path: "/",
+      sameSite: "lax",
+      secure: true,
+    });
+
+    return ok(null);
+  }
+
+  const reason = error.reason;
+  return err({ reason });
 };
 
 const loginUserAction = async (user: unknown) => {
-  const validatedData = safeParse(loginFormSchema, user);
-  if (!validatedData.success) return;
-
   const reqHeaders = await headers();
   const meta = {
     ipAddress: reqHeaders.get("X-Forwarded-For")?.split(",")[0].trim() ?? null,
     userAgent: reqHeaders.get("User-Agent"),
   };
 
-  const session = await authService.login(validatedData.output, meta);
-  if (!session) return;
+  const [error, session] = await authService.login(user, meta);
 
-  const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
-  const cookieStore = await cookies();
-  cookieStore.set("session", session, {
-    expires: expiresAt,
-    httpOnly: true,
-    path: "/",
-    sameSite: "lax",
-    secure: true,
-  });
+  if (!error) {
+    const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+    const cookieStore = await cookies();
+    cookieStore.set("session", session, {
+      expires: expiresAt,
+      httpOnly: true,
+      path: "/",
+      sameSite: "lax",
+      secure: true,
+    });
+
+    return ok(null);
+  }
+
+  const reason = error.reason;
+  return err({ reason });
 };
 
 const logoutUserAction = async () => {
   const cookieStore = await cookies();
   const rawSession = cookieStore.get("session")?.value;
 
-  if (!rawSession) return;
+  if (!rawSession) return err({ reason: "SESSION_MISSING" });
 
-  const result = await authService.logout(rawSession);
+  const [error] = await authService.logout(rawSession);
+  cookieStore.delete("session");
 
-  if (result.success) {
-    cookieStore.delete("session");
+  if (error) {
+    const reason = error.reason;
+    return err({ reason });
   }
+
+  return ok(null);
 };
 
 const isUsernameExist = async (username: string) => {
