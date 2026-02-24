cba4aea1b4a97aa1c49902fbdb3a335a00c5f808
Ali Gheisar <ali.gheisar.dev@gmail.com>
2026-02-24 13:01:29 +0330

    add error handling utilities.

---
+++ src/utils/error.ts
@@ -0,0 +1,21 @@
+type Result<S, E extends { reason: string }> = [E, null] | [null, S];
+
+const ok = <S>(data: S): Result<S, never> => {
+  return [null, data];
+};
+
+const err = <const R extends string, E extends { reason: R }>(
+  error: E,
+): Result<never, E> => {
+  return [error, null];
+};
+
+const handleError = <R extends string>(
+  reason: R,
+  handlers: Record<R, () => void>,
+) => {
+  const handler = handlers[reason];
+  handler();
+};
+
+export { ok, err, handleError };
