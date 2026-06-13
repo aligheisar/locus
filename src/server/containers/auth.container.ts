dc260104f0c1d0cf1ad64285811f26cc86b6a19b
Ali Gheisar <ali.gheisar.dev@gmail.com>
2026-06-13 14:16:44 +0330

    moving session related container logic to it own file.

--- src/server/containers/auth.container.ts
+++ src/server/containers/auth.container.ts
@@ -1,20 +1,15 @@
+import { sessionService } from "@/server/containers/session.container";
 import { AccountRepository } from "@/server/repositories/account.repository";
 import { AuthRepository } from "@/server/repositories/auth.repository";
 import { ProfileRepository } from "@/server/repositories/profile.repository";
-import { SessionRepository } from "@/server/repositories/session.repository";
 import { UserRepository } from "@/server/repositories/user.repository";
 import { AuthService } from "@/server/services/auth.service";
 import { PasswordService } from "@/server/services/password.service";
-import { SessionService } from "@/server/services/session.service";
-import { TokenService } from "@/server/services/token.service";
 
 const authRepo = new AuthRepository();
 const accountRepo = new AccountRepository();
 const profileRepo = new ProfileRepository();
 const userRepo = new UserRepository();
-const sessionRepo = new SessionRepository();
-const tokenService = new TokenService();
-const sessionService = new SessionService(sessionRepo, tokenService);
 const passwordService = new PasswordService();
 
 const authService = new AuthService(
