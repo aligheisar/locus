59551632c898239050cc1601ac70e8da22e9f675
Ali Gheisar <ali.gheisar.dev@gmail.com>
2026-06-13 20:29:14 +0330

    adding profile page.

---
+++ src/app/(nav)/profile/page.tsx
@@ -0,0 +1,13 @@
+import { SettingsLink } from "@/features/profile/components/SettingsLink";
+
+const ProfilePage = () => {
+  return (
+    <div>
+      <section>
+        <SettingsLink />
+      </section>
+    </div>
+  );
+};
+
+export default ProfilePage;
