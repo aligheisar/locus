71efd37d003109c47598bad3cf1ed98f08ad9d50
Ali Gheisar <ali.gheisar.dev@gmail.com>
2026-06-29 11:57:51 +0330

    update session page.
    update layout and remove revoked session temporary.

--- src/app/(raw)/profile/settings/sessions/page.tsx
+++ src/app/(raw)/profile/settings/sessions/page.tsx
@@ -1,29 +1,32 @@
 import { Suspense } from "react";
 
-import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
 
 import { ActiveSessions } from "@/features/sessions/components/ActiveSessions";
 import { CurrentSession } from "@/features/sessions/components/CurrentSession";
-import { RevokedSessions } from "@/features/sessions/components/RevokedSessions";
+import { SectionWrapper } from "@/features/sessions/components/SectionWrapper";
 
 const SessionsPage = () => {
   return (
-    <section>
-      <Suspense fallback={<p>loading current session ...</p>}>
-        <CurrentSession />
-      </Suspense>
+    <section className="grid gap-2">
+      <SectionWrapper>
+        <h2>Current Session</h2>
+        <Suspense fallback={<p>loading current session ...</p>}>
+          <CurrentSession />
+        </Suspense>
+      </SectionWrapper>
 
-      <Separator />
-
-      <Suspense fallback={<p>loading sessions ...</p>}>
-        <ActiveSessions />
-      </Suspense>
-
-      <Separator />
-
-      <Suspense fallback={<p>loading revoked session ...</p>}>
-        <RevokedSessions />
-      </Suspense>
+      <SectionWrapper>
+        <div className="flex items-center justify-between">
+          <h2>Active Sessions</h2>
+          <Button size="sm" variant="destructive">
+            Revoke All
+          </Button>
+        </div>
+        <Suspense fallback={<p>loading sessions ...</p>}>
+          <ActiveSessions />
+        </Suspense>
+      </SectionWrapper>
     </section>
   );
 };
