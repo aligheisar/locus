91c23824af574d8b440bc347df8a36240c30ffc2
Ali Gheisar <ali.gheisar.dev@gmail.com>
2026-06-29 11:55:32 +0330

    use session item and item group in active session.

--- src/features/sessions/components/ActiveSessions.tsx
+++ src/features/sessions/components/ActiveSessions.tsx
@@ -1,13 +1,18 @@
+import { ItemGroup } from "@/components/ui/item";
+
 import { getActiveSessionsAction } from "@/server/actions/session.action";
+
+import { SessionItem } from "@/features/sessions/components/SessionItem";
 
 const ActiveSessions = async () => {
   const sessions = await getActiveSessionsAction();
-
-  return sessions.map((item) => (
-    <p key={item.id}>
-      {Intl.DateTimeFormat("en", { dateStyle: "full" }).format(item.expiresAt)}
-    </p>
-  ));
+  return (
+    <ItemGroup className="gap-2">
+      {sessions.map((item) => (
+        <SessionItem key={item.id} session={item} variant="outline" />
+      ))}
+    </ItemGroup>
+  );
 };
 
 export { ActiveSessions };
